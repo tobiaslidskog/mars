e14ffdd7475669b1db6ee3c68279c3efa71556b0
Tobias Lidskog <tobias.lidskog@cybercom.com>
2011-12-07 14:34:29 +0100

    Revert "Break test case, to verify Jenkins test run"
    This reverts commit f9b8fd0f8eb4bdb6aef1751e7740a00569384ed3.

--- core/src/test/java/com/cybercom/mars/MathUtilsTest.java
+++ core/src/test/java/com/cybercom/mars/MathUtilsTest.java
@@ -4,6 +4,6 @@ public class MathUtilsTest {
   @org.junit.Test
   public void testSum() throws Exception {
     MathUtils utils = new MathUtils();
-    assert 43 == utils.sum(24, 18);
+    assert 42 == utils.sum(24, 18);
   }
 }
